c98f5fc6a3aab8ead07d9a3fed46ff4b45940aa5
dylan <dylanleodaniel@gmail.com>
2023-09-20 00:22:07 -0400

    New

---
+++ lua/cmake-build-debug/CMakeFiles/lua.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lua.dir/main.cpp.obj"
+  "CMakeFiles/lua.dir/main.cpp.obj.d"
+  "liblua.dll.a"
+  "lua.exe"
+  "lua.exe.manifest"
+  "lua.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lua.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
